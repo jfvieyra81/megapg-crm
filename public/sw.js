3578be2da4a7df4d02553f1975d9f9507a91fbab
jfvieyra81 <jfvieyra@gmail.com>
2026-04-11 08:18:13 -0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'megapg-v5.10';
+const CACHE = 'megapg-v5.11';
 self.addEventListener('install',e=>{self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
